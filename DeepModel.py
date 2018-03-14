ced759c742e2fa203b51993cbd570884e2201567
mehulved77 <mehulved@hotmail.com>
2018-03-14 15:53:19 +0530

    Adds Network Summary

--- DeepModel.py
+++ DeepModel.py
@@ -83,10 +83,12 @@ y_train
 def larger_cnn_model():
     # create model
     model = Sequential()
-    model.add(Conv2D(30, (5, 5), input_shape=input_shape, activation='relu'))
+    model.add(Conv2D(30, (3, 3), input_shape=input_shape, activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(15, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(15, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2))) 
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
